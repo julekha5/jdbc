20d4fc8a07c7e336b6ac4456165c778104ec7abf
Julekha5 <mulani.julekha93@gmail.com>
2022-01-08 18:47:32 +0530

    Added : Database created and connected to database successfullly

---
+++ src/main/java/com/bridgelabz/jdbc/DBConnection.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.jdbc;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static void main(String[] args) {
+        //step 1 Load the class driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            System.out.println("driver is not loaded");
+        }
+
+        //step 2 create connection
+        String JDBCURL = "jdbc:mysql://localhost:3307/emp_payroll_service";
+        try {
+            DriverManager.getConnection(JDBCURL, "root", "root");
+            System.out.println("Connection is established");
+        } catch (SQLException e) {
+            System.out.println("connection not established");
+        }
+    }
+}
