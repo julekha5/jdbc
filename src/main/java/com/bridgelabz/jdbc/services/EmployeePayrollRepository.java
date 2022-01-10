6a2a78d1190dc3bf64c1bd53584323d219752ee3
Julekha5 <mulani.julekha93@gmail.com>
2022-01-10 19:37:24 +0530

    Added : Databse function use sum, avg,min,max,count

--- src/main/java/com/bridgelabz/jdbc/services/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/jdbc/services/EmployeePayrollRepository.java
@@ -4,7 +4,9 @@ import com.bridgelabz.jdbc.model.EmployeeInfo;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EmployeePayrollRepository {
 
@@ -19,6 +21,7 @@ public class EmployeePayrollRepository {
         }
         return connection;
     }
+
     //retrieve data of all employee
     public List<EmployeeInfo> retrieveData() {
         List<EmployeeInfo> employeeInfo = new ArrayList<>();
@@ -59,12 +62,12 @@ public class EmployeePayrollRepository {
     }
 
     //update salary using prepared statement
-    public void updateSalaryUsingPreparedStatement(String name, int basic_pay){
+    public void updateSalaryUsingPreparedStatement(String name, int basic_pay) {
         try (Connection connection = getConnection()) {
             String query = "update employee_payroll set basic_pay = ? where name = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,basic_pay);
-            preparedStatement.setString(2,name);
+            preparedStatement.setInt(1, basic_pay);
+            preparedStatement.setString(2, name);
             int result = preparedStatement.executeUpdate();
             if (result >= 1) {
                 System.out.println("salary updated");
@@ -73,14 +76,14 @@ public class EmployeePayrollRepository {
             e.printStackTrace();
         }
     }
+
     //retrieve data of employee of particular date range
     public List<EmployeeInfo> retrieveDataByDateRange() {
         List<EmployeeInfo> employeeInfo = new ArrayList<>();
         try (Connection connection = getConnection()) {
             //step 3
             Statement statement = connection.createStatement();
-            String sqlQuery = "select * from employee_payroll\n" +
-                    "where startDate between '2021-04-10' and date(now());\n";
+            String sqlQuery = String.format("select * from employee_payroll where startDate between '2021-04-10' and date(now())");
             ResultSet resultset = statement.executeQuery(sqlQuery);
             while (resultset.next()) {
                 EmployeeInfo info = new EmployeeInfo();
@@ -96,5 +99,100 @@ public class EmployeePayrollRepository {
             e.printStackTrace();
         }
         return employeeInfo;
+    }
+
+    //Sum of salary by gender;
+    public Map<String, Double> getSumOfSalaryByGender() {
+        Map<String, Double> sumOfSalary = new HashMap<>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String query = "select gender,sum(basic_pay) as sum_salary from employee_payroll group by gender;";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                EmployeeInfo info = new EmployeeInfo();
+                String gender = String.valueOf(resultset.getString("gender").charAt(0));
+                Double sum_salary = Double.valueOf(resultset.getString("sum_salary"));
+                sumOfSalary.put(gender, sum_salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sumOfSalary;
+    }
+
+    //average salary by gender
+    public Map<String, Double> getAverageSalaryByGender() {
+        Map<String, Double> avgSalary = new HashMap<>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String query = "select gender,avg(basic_pay) as avg_salary from employee_payroll group by gender;";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                EmployeeInfo info = new EmployeeInfo();
+                String gender = String.valueOf(resultset.getString("gender").charAt(0));
+                Double avg_salary = Double.valueOf(resultset.getString("avg_salary"));
+                avgSalary.put(gender, avg_salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return avgSalary;
+    }
+
+    //maximum salary by gender
+    public Map<String, Double> getMaxSalaryByGender() {
+        Map<String, Double> maxSalary = new HashMap<>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String query = "select gender,max(basic_pay) as max_salary from employee_payroll group by gender;";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                EmployeeInfo info = new EmployeeInfo();
+                String gender = String.valueOf(resultset.getString("gender").charAt(0));
+                Double max_salary = Double.valueOf(resultset.getString("max_salary"));
+                maxSalary.put(gender, max_salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return maxSalary;
+    }
+
+    //mimimum salary by gender
+    public Map<String, Double> getMinSalaryByGender() {
+        Map<String, Double> minSalary = new HashMap<>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String query = "select gender,min(basic_pay) as min_salary from employee_payroll group by gender;";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                EmployeeInfo info = new EmployeeInfo();
+                String gender = String.valueOf(resultset.getString("gender").charAt(0));
+                Double min_salary = Double.valueOf(resultset.getString("min_salary"));
+                minSalary.put(gender, min_salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return minSalary;
+    }
+
+    //count by gender
+    public Map<String, Integer> getCountByGender() {
+        Map<String, Integer> countByGender = new HashMap<>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String query = "select gender,count(*) as count from employee_payroll group by gender";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                EmployeeInfo info = new EmployeeInfo();
+                String gender = String.valueOf(resultset.getString("gender").charAt(0));
+                Integer count = Integer.valueOf(resultset.getString("count"));
+                countByGender.put(gender, count);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return countByGender;
     }
 }
--- src/main/java/com/bridgelabz/jdbc/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc/services/EmployeePayrollService.java
@@ -6,23 +6,53 @@ public class EmployeePayrollService {
 
     EmployeePayrollRepository repository = new EmployeePayrollRepository();
 
-    public static void main(String[] args) {
-        EmployeePayrollService service = new EmployeePayrollService();
-        service.retrieveData();
-        service.updateSalary("Julekha", 4000000);
-        service.retrieveDataByDate();
-    }
-
-    private void retrieveData() {
+    public void retrieveData(String sqlQuery) {
         System.out.println(repository.retrieveData());
     }
 
-    private void updateSalary(String name, int basic_pay) {
+    public void updateSalary(String name, int basic_pay) {
+        repository.updateSalary(name.toLowerCase(Locale.ROOT), basic_pay);
+    }
+
+    public void updateSalaryUsingPreparedStatement(String name, int basic_pay) {
         repository.updateSalaryUsingPreparedStatement(name.toLowerCase(Locale.ROOT), basic_pay);
     }
 
-    private void retrieveDataByDate() {
+    public void retrieveDataByDate(String sqlQuery) {
         System.out.println(repository.retrieveDataByDateRange());
     }
 
+    public void getSumOfSalaryByGender(String sqlQuery){
+        System.out.println(repository.getSumOfSalaryByGender());
+    }
+
+    public void getAverageSalaryByGender(String sqlQuery){
+        System.out.println(repository.getAverageSalaryByGender());
+    }
+
+    public void getMaxSalaryByGender(String sqlQuery) {
+        System.out.println(repository.getMaxSalaryByGender());
+    }
+
+    public void getMinSalaryByGender(String sqlQuery){
+        System.out.println(repository.getMinSalaryByGender());
+    }
+
+    public void getCountByGender(String sqlQuery){
+        System.out.println(repository.getCountByGender());
+    }
+
+//    public static void main(String[] args) {
+//        EmployeePayrollService service = new EmployeePayrollService();
+//        String sqlQuery = null;
+//        service.retrieveData(null);
+//        service.updateSalary("Julekha", 4000000);
+//        service.updateSalaryUsingPreparedStatement("Kajal", 6200000);
+//        service.retrieveDataByDate(null);
+//        service.getSumOfSalaryByGender(null);
+//        service.getAverageSalaryByGender(null);
+//        service.getMaxSalaryByGender(null);
+//        service.getMinSalaryByGender(null);
+//        service.getCountByGender(null);
+//    }
 }
